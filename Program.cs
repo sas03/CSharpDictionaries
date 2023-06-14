a2b82de6415a27bd028dcaf0571da6517462496a
sas03 <stephanesumo@yahoo.fr>
2023-06-14 15:23:16 +0200

    dictionaries

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿namespace Dictionary;
+class Program
+{
+    public static void Display(Dictionary<char, List<string>> dictionary){
+        foreach(KeyValuePair<char, List<string>> f in dictionary){
+            foreach(string s in f.Value){
+                Console.WriteLine("Key: {0}, Value: {1}", f.Key, s);
+            }
+        }
+    }
+    static void Main(string[] args)
+    {
+        Dictionary<char, List<string>> dictionary = new Dictionary<char, List<string>>();
+        dictionary['a'] = new List<string> {"arc", "alabasta", "ami"};
+        dictionary['b'] = new List<string> {"banane", "buisson", "but"};
+        dictionary.Add('c', new List<string> {"chat", "cerise", "carreau"});
+
+        Display(dictionary);
+
+        Console.WriteLine("The current size of the dictionary is: " + dictionary.Count);
+
+        dictionary.Remove('c');
+        Display(dictionary);
+        Console.WriteLine("The current size of the dictionary is: " + dictionary.Count);
+    }
+}
